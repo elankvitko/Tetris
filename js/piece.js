c2dad502597655a30a8fe9c5d4f8744ac008430a
Elan Kvitko <elan@idknyc.com>
2016-06-28 15:37:03 -0400

    finish piece

--- js/piece.js
+++ js/piece.js
@@ -35,13 +35,47 @@ function Piece( type ) {
   this.currentCoordinates = [ 2,5 ];
   this.previewCoordinates = [ 2,2 ];
   this.buildSquares();
-  this.trace = this.setTrace();
-  this.previewTrace = this.setPreviewTrace();
+  this.trace = this.trace();
+  this.previewTrace = this.previewTrace();
 }
 
 Piece.prototype.buildSquares() = function() {
   var color = this.color;
-  this.squares = this.typeCoordinates.mpa( function( coordinates ) {
-    return new Square( coordinates, color );
+  this.squares = this.typeCoordinates.mpa( function( position ) {
+    return new Square( position, color );
   });
+};
+
+Piece.prototyope.trace = function() {
+  return [ this.currentCoordinates, this.squares[ 1 ].boardPosition( this.currentCoordinates ), this.squares[ 2 ].boardPosition( this.currentCoordinates ), this.squares[ 3 ].boardPosition( this.currentCoordinates ) ];
+};
+
+Piece.prototyope.previewTrace = function() {
+  return [ this.previewCoordinates, this.squares[ 1 ].boardPosition( this.previewCoordinates ), this.squares[ 2 ].boardPosition( this.previewCoordinates ), this.squares[ 3 ].boardPosition( this.previewCoordinates ) ];
+};
+
+Piece.prototype.goLeft = function() {
+  this.currentCoordinates[ 1 ]--;
+  this.trace = this.trace();
+};
+
+Piece.prototype.goRight = function() {
+  this.currentCoordinates[ 1 ]++;
+  this.trace = this.trace();
+};
+
+Piece.prototype.goDown = function() {
+  this.currentCoordinates[ 0 ]++;
+  this.trace = this.trace();
+};
+
+Piece.prototype.rotate = function() {
+  this.squares.forEach( function( square ) {
+    square.rotate();
+  });
+  this.trace = this.trace();
+};
+
+Piece.prototype.possibleTrace = function() {
+  return [ this.currentCoordinates, this.squares[ 1 ].nextBoardPosition( this.currentCoordinates ), this.squares[ 2 ].nextBoardPosition( this.currentCoordinates ), this.squares[ 3 ].nextBoardPosition( this.currentCoordinates ) ];
 };
