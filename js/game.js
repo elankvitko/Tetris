467b30bc286d9a2ffda0d43fac648ee9af8ade71
Elan Kvitko <elan@idknyc.com>
2016-06-29 23:44:27 -0400

    fix bugs and game board - now working version of game

--- js/game.js
+++ js/game.js
@@ -1,4 +1,9 @@
 $( document ).ready( function() {
+
+  $( '#begin' ).on( 'click', function( event ) {
+    game.startGame();
+  });
+
   function Game() {
     this.board = new Board();
     this.boardDisplay = new BoardContainer();
@@ -10,13 +15,35 @@ $( document ).ready( function() {
     this.difficulty = 0;
     this.currentPiece = this.newPiece();
     this.nextPiece = this.newPiece();
-    this.interval = null;
-  };
+    this.interval = null
+  }
 
-  var types = [ "L", "J", "T", "LINE", "Z", "S", "SQUARE" ];
+  var types = [ "L", "J", "T", "LINE", "Z", "S", "SQUARE" ]
 
   Game.prototype.newPiece = function() {
     return new Piece( types[ Math.floor( Math.random() * types.length ) ], "red" );
+  };
+
+  var game = new Game();
+
+  Game.prototype.updateTime = function() {
+    $( '#timer' ).html( this.secondsRunning() );
+  };
+
+  Game.prototype.scorePoints = function() {
+    this.score += ( 10 * this.board.fullRows().length * this.board.fullRows().length );
+  };
+
+  Game.prototype.updateScore = function() {
+    $( '#score' ).html( this.score );
+  };
+
+  Game.prototype.updateLevel = function() {
+    $( '#level' ).html( this.difficulty + 1);
+  };
+
+  Game.prototype.secondsRunning = function() {
+    return Math.floor( this.timer / this.frameRate );
   };
 
   Game.prototype.refreshBoard = function() {
@@ -27,30 +54,15 @@ $( document ).ready( function() {
     this.scorePoints();
     this.updateScore();
     this.board.breakFullRows( this.board );
-  }
+  };
 
   Game.prototype.refreshBoardPreview = function() {
     this.board.clearNextPiece();
     this.board.addNextPiece( this.nextPiece );
-    this.boardPreview.empty( this.board.nextPreview );
+    this.boardPreview.empty( this.board.nextPreview) ;
     this.boardPreview.render( this.board.nextPreview, this.nextPiece );
   };
 
-  Game.prototype.scorePoints = function() {
-    this.score += ( 10 * this.board.fullRows().length * this.board.fullRows().length );
-  };
-
-  Game.prototype.updateScore = function() {
-    $( '#score' ).html( this.score );
-  }
-
-  Game.prototype.secondsRunning = function() {
-    return Math.floor( this.timer / this.frameRate );
-  };
-
-  Game.prototype.updateTime = function() {
-    $( '#timer' ).html( this.secondsRunning() );
-  };
 
   Game.prototype.loop = function() {
     if ( !this.board.checkTop( this.board ) ) {
@@ -70,6 +82,15 @@ $( document ).ready( function() {
           this.refreshBoardPreview();
         };
       };
+
+      if ( this.timer % 2500 === 0 && this.difficulty < 14 ) {
+        this.difficulty++
+        this.updateLevel();
+      };
+
+      this.refreshBoard();
+    } else {
+      this.endGame()
     };
   };
 
@@ -78,33 +99,35 @@ $( document ).ready( function() {
     this.interval = setInterval( this.loop.bind( this ), 1000 / this.frameRate );
   };
 
-  var game = new Game();
+  Game.prototype.endGame = function() {
+    clearInterval( this.interval );
+    $( '#hidden' ).show();
+  };
 
-  $( document ).on( 'keyup', function( event ) {
+  $(document).on('keyup', function(event) {
     event.preventDefault();
 
-    if ( event.keyCode == 37 && game.board.canGoLeft( game.currentPiece ) ) {
+    if( event.keyCode == 37 && game.board.canGoLeft( game.currentPiece ) ) {
       game.currentPiece.goLeft();
     };
 
-    if ( event.keyCode == 38 && game.board.canRotate( game.currentPiece ) ) {
+    if( event.keyCode == 38 && game.board.canRotate( game.currentPiece ) ) {
       game.currentPiece.rotate();
     };
-    if ( event.keyCode == 39 && game.board.canGoRight( game.currentPiece ) ) {
+
+    if( event.keyCode == 39 && game.board.canGoRight( game.currentPiece ) ) {
       game.currentPiece.goRight();
     };
-    if ( event.keyCode == 40 && game.board.canGoDown( game.currentPiece ) ) {
-      game.currentPiece.goDown();
+
+    if( event.keyCode == 40 && game.board.canGoDown( game.currentPiece ) ) {
+       game.currentPiece.goDown();
     };
-    if ( event.keyCode == 32 && game.board.canGoDown( game.currentPiece ) ) {
-      while( game.board.canGoDown( game.currentPiece ) ) {
+
+    if( event.keyCode == 32 && game.board.canGoDown( game.currentPiece ) ) {
+      while ( game.board.canGoDown( game.currentPiece ) ) {
         game.currentPiece.goDown();
       };
     };
     game.refreshBoard();
-  });
-
-  $( '#begin' ).on( 'click', function( event ) {
-    game.startGame();
   });
 });
